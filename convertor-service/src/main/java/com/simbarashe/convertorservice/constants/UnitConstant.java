7130929b2ba4dc804c59c1fa3777f3d86039114d
simba84-cloud <simbamuzika@gmail.com>
2022-02-13 21:14:44 +0200

    Create constants

---
+++ convertor-service/src/main/java/com/simbarashe/convertorservice/constants/UnitConstant.java
@@ -0,0 +1,9 @@
+package com.simbarashe.convertorservice.constants;
+
+public class UnitConstant {
+    public static final String KELVIN = "Kelvin";
+    public static final String CELSIUS = "Celsius";
+    public static final String KILOMETRES = "Kilometres";
+    public static final String MILES = "Miles";
+
+}
